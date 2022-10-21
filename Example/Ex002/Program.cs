5b97b580e0f120badd627c44e961f3f56ff2097a
Konstantin <rodin.konstantin2014@yandex.ru>
2022-10-21 22:34:15 +1000

    Ex002

---
+++ Example/Ex002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите любое число");
+string AnyNumberStr = Console.ReadLine();
+string stringAnyNumberStr = Convert.ToString(AnyNumberStr);
+if (AnyNumberStr.Length > 2) 
+{
+    Console.WriteLine("Третья цифра " + AnyNumberStr[2]);
+}  
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
