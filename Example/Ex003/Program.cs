209a544b0478b74bcd6d55db2314acdf70a46ca8
Konstantin <rodin.konstantin2014@yandex.ru>
2022-10-21 23:05:23 +1000

    Ex003

---
+++ Example/Ex003/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите любое число, обозначающее день недели");
+string AnyDayStr = Console.ReadLine();
+int stringAnyDayStr = Convert.ToInt32(AnyDayStr);
+
+void CheckTheDayWeek (int stringAnyDayStr) 
+{
+    if (stringAnyDayStr == 6 || stringAnyDayStr == 7)
+    {
+        Console.WriteLine("Этот день выходной");
+    }
+    else if (stringAnyDayStr < 1 || stringAnyDayStr > 7) 
+    {
+        Console.WriteLine("Не является днем недели");
+    }
+    else Console.WriteLine("Этот день не выходной");   
+    }
+CheckTheDayWeek (stringAnyDayStr); 
