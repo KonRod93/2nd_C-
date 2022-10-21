fd1a397c56c270f62be54769e8a2167f4f29f59f
Konstantin <rodin.konstantin2014@yandex.ru>
2022-10-21 22:22:42 +1000

    Ex001

---
+++ Example/Ex001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+string threeDigitNumberStr = Console.ReadLine();
+string stringNumberStr = Convert.ToString(threeDigitNumberStr);
+Console.WriteLine("Вторая цифра числа "+stringNumberStr[1]);
